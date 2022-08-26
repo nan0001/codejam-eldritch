13665a038752cc2ed4135bc6b26ea05d2de8bd61
Nastya <himeriya97@mgmail.com>
2022-08-26 14:08:45 +0300

    feat: add difficulties

--- codejam-eldritch/script.js
+++ codejam-eldritch/script.js
@@ -1,5 +1,6 @@
 const ancientCards = document.querySelectorAll('.ancient-card');
 let activeAncient = {};
+let activeDifficulty = {};
 let lastCard = document.createElement('div');
 let deck = document.createElement('div');
 deck.classList.add('deck');
@@ -7,15 +8,21 @@ lastCard.classList.add('last-card');
 const deckContainer = document.querySelector('.deck-container');
 let shuffleButton = document.createElement('button');
 let cardDeck = [];
-const dots = document.querySelectorAll('.dot')
+const dots = document.querySelectorAll('.dot');
+const difficultyButtons = document.querySelectorAll('.difficulty');
 
 import brownCards from './data/mythicCards/brown/index.js';
 import blueCards from './data/mythicCards/blue/index.js';
 import greenCards from './data/mythicCards/green/index.js';
 import ancientsData from './data/ancients.js';
+import difficulties from './data/difficulties.js';
 
 for (let i = 0; i < ancientCards.length; i++){
     ancientCards[i].style.backgroundImage = `url(${ancientsData[i].cardFace})`
+}
+
+for (let i = 0; i < difficulties.length; i++){
+    difficultyButtons[i].innerHTML = difficulties[i].name
 }
 
 shuffleButton.classList.add('shuffle-button')
@@ -24,6 +31,12 @@ shuffleButton.innerHTML = 'Замешать колоду';
 ancientCards.forEach((val)=>{
     val.addEventListener('click',() => {
         toggleActive(val);
+    })
+})
+
+difficultyButtons.forEach((val)=>{
+    val.addEventListener('click',() => {
+        toggleDifficulty(val);
     })
 })
 
@@ -39,6 +52,22 @@ function toggleActive(val){
             value.classList.remove('active')
         })
         val.classList.add('active');
+        difficultyButtons.forEach((value)=>{
+            value.classList.remove('chosen')
+        })
+        deckContainer.innerHTML = ''
+    }
+}
+
+function toggleDifficulty(val){
+    if (val.classList.contains('chosen')){
+        return
+    }
+    else{
+        difficultyButtons.forEach((value)=>{
+            value.classList.remove('chosen')
+        })
+        val.classList.add('chosen');
         deckContainer.innerHTML = ''
         deckContainer.append(shuffleButton);
         dots.forEach((val)=> {val.innerHTML = ''})
@@ -64,14 +93,32 @@ function findActiveAncient(){
     })
 }
 
+function findActiveDifficulty(){
+    let activeDifficultyName = '';
+    difficultyButtons.forEach((value)=>{
+        if (value.classList.contains('chosen')){
+            activeDifficultyName = value.innerHTML;
+        }
+    })
+    difficulties.forEach((val)=>{
+        if (val.name === activeDifficultyName){
+            activeDifficulty = val
+        }
+    })
+}
+
 function createDeck(){
     findActiveAncient();
+    findActiveDifficulty();
     const greenAmount = activeAncient.firstStage.greenCards + activeAncient.secondStage.greenCards + activeAncient.thirdStage.greenCards
     const brownAmount = activeAncient.firstStage.brownCards + activeAncient.secondStage.brownCards + activeAncient.thirdStage.brownCards
     const blueAmount = activeAncient.firstStage.blueCards + activeAncient.secondStage.blueCards + activeAncient.thirdStage.blueCards
-    let greenDeck = getCardArray(greenCards,greenAmount);
-    let brownDeck = getCardArray(brownCards, brownAmount);
-    let blueDeck = getCardArray(blueCards,blueAmount);
+    let greenCardsDif = getDifficultyArray(greenCards, activeDifficulty, greenAmount);
+    let brownCardsDif = getDifficultyArray(brownCards, activeDifficulty, brownAmount);
+    let blueCardsDif = getDifficultyArray(blueCards, activeDifficulty, blueAmount);
+    let greenDeck = getCardArray(greenCardsDif,greenAmount);
+    let brownDeck = getCardArray(brownCardsDif, brownAmount);
+    let blueDeck = getCardArray(blueCardsDif,blueAmount);
     let firstStageArr = [getCardArray(greenDeck,activeAncient.firstStage.greenCards), getCardArray(brownDeck,activeAncient.firstStage.brownCards),getCardArray(blueDeck,activeAncient.firstStage.blueCards)]
     greenDeck = filterOneDeck (greenDeck, firstStageArr, 0)
     brownDeck = filterOneDeck (brownDeck, firstStageArr, 1)
@@ -125,6 +172,7 @@ function shuffleCards(){
     deckContainer.append(deck)
     deckContainer.append(lastCard)    
     trackCards()
+    deck.style.display = ''
 }
 
 function switchCard(){
@@ -164,6 +212,55 @@ function filterOneDeck (deck, stageArr, ind){
 
 function getRandomCard(array){
     return array[getRandomInd(array.length)]
+}
+
+function getDifficultyArray(cardsArray, difficulty, amount){
+    let newArr = [];
+    let filteredArray = [];
+    let count = 0;
+    switch (difficulty.id) {
+        case 'very easy':
+            cardsArray.forEach((val)=>{
+                if (val.difficulty === 'easy'){
+                    count += 1
+                    newArr.push(val)
+                }
+            })
+            if (count < amount){
+                filteredArray = cardsArray.filter(val => val.difficulty === 'normal')
+                for (let i = 1; i <= amount - count; i++){
+                    let randomCard = getRandomCard(filteredArray);
+                    filteredArray = filteredArray.filter(val => val !== randomCard)
+                    newArr.push(randomCard)
+                }
+            }
+            return newArr;
+        case 'easy':
+            newArr = cardsArray.filter(val => val.difficulty !== 'hard')
+            return newArr;
+        case 'normal': 
+            newArr = cardsArray
+            return newArr;
+        case 'hard':             
+            newArr = cardsArray.filter(val => val.difficulty !== 'easy')
+            return newArr;
+        case 'very hard': 
+            cardsArray.forEach((val)=>{
+                if (val.difficulty === 'hard'){
+                    count += 1
+                    newArr.push(val)
+                }
+            })
+            if (count < amount){
+                filteredArray = cardsArray.filter(val => val.difficulty === 'normal')
+                for (let i = 1; i <= amount - count; i++){
+                    let randomCard = getRandomCard(filteredArray);
+                    filteredArray = filteredArray.filter(val => val !== randomCard)
+                    newArr.push(randomCard)
+                }
+            }
+            return newArr;
+    }
 }
 
 function getCardArray(cardsArray, amount) {
